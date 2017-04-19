37c6f3a5c84b216b4a399cc30f243b1d5bf4cc54
Vetedde <vetedde@narod.ru>
2017-04-19 12:26:38 +0300

    Upadate dist

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  var link = document.createElement("a");
+  link.setAttribute("href","http://sbmlviewer.insilicobio.ru/"+dist+"viewer.xhtml");
+  link.innerHTML = "Try demo online";
+  
+  document.getElementById("tryDemo").appendChild(link);
+  var liList = document.getElementsByTagName("ul")[1].getElementsByTagName("li");
+  for(var i = 0; i < liList.length; i++) {
+    var Link = liList[i].getElementsByTagName("a")[0].getAttribute("href");
+    var newLink = "http://sbmlviewer.insilicobio.ru/"+dist+"viewer.xhtml"+Link.match(/(\?[\w\:\/\.\-]+)/)[0];
+    liList[i].getElementsByTagName("a")[0].setAttribute("href",);
+  }
+}
