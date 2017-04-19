d81db9313915e10db3047d67ca62a583680cd357
Vetedde <vetedde@narod.ru>
2017-04-19 16:05:13 +0300

    Add btn "try demo"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 window.onload = function() {
-  var link = document.createElement("a");
-  link.setAttribute("href","http://sbmlviewer.insilicobio.ru/"+dist+"viewer.xhtml");
-  link.innerHTML = "Try demo online";
   
-  document.getElementById("tryDemo").appendChild(link);
+  document.getElementById("demoLink").setAttribute("href","http://sbmlviewer.insilicobio.ru/"+dist+"viewer.xhtml");
+  
   var liList = document.getElementsByTagName("ul")[1].getElementsByTagName("li");
   for(var i = 0; i < liList.length; i++) {
     var Link = liList[i].getElementsByTagName("a")[0].getAttribute("href");
