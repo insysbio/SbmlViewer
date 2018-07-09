61114bc1b6205282a4e052cc8da5fa86cb4bd971
Victoria Tkachenko <tkachenko@insysbio.ru>
2018-07-09 10:44:44 +0300

    annotation

--- src/components/annotation-side/annotation-side.vue
+++ src/components/annotation-side/annotation-side.vue
@@ -31,42 +31,12 @@ export default {
     documentToString: (doc) => {
       let container = document.createElement('div').appendChild(doc.firstElementChild)
       return container.innerHTML
+    },
+    onAnnotationClose: function () {
+      this.$emit('closeAnnotation')
     }
   }
 }
-/*
-
-  clearErrMess()
-
-  console.log('Display information about element...')
-  // element, that was clicked
-  var id = event.target.id, sideContent = document.getElementById('sideContent')
-
-  console.log(' Clear display...')
-  while (sideContent.childNodes[0]) {
-    sideContent.removeChild(sideContent.childNodes[0])
-  }
-  console.log('   Success')
-
-  console.log(' Transform data to document...')
-  xsltProcessor2.setParameter(null, 'elementId', id)
-  try {
-    var resultDocument = xsltProcessor2.transformToDocument(curFile['content'])
-    sideContent.appendChild(resultDocument.firstElementChild)
-    console.log('   Success\n Success display')// Transform
-  } catch (err) {
-    console.error(' Err:', err)
-    var p = document.createElement('p')
-    p.classList.add('class', 'w3-text-red', 'w3-center', 'w3-large', 'w3-padding')
-    p.appendChild(document.createTextNode('Cannot display element'))
-    sideContent.appendChild(p)
-  }
-
-  // show block
-  document.getElementById('sideInformBlock').style.display = 'block'
-  document.getElementById('sideInformBlock').classList.add('w3-col', 'l3', 'm3', 's3', 'w3-animate-right')
-  document.getElementById('mainContent').classList.add('l9', 'm9', 's9')
-
-  // update equations
-  MathJax.Hub.Queue(['Typeset', MathJax.Hub]) */
+// update equations
+// MathJax.Hub.Queue(['Typeset', MathJax.Hub]) */
 </script>
