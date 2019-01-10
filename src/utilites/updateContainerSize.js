a3822b12fdc570307060294c7560502e1ffa5f6d
Victoria Tkachenko <tkachenko@insysbio.ru>
2019-01-10 14:07:38 +0300

    scroll

--- src/utilites/updateContainerSize.js
+++ src/utilites/updateContainerSize.js
@@ -1,17 +1,16 @@
 export function updateContainerSize () {
-  console.log('size')
   let newMargin = document.getElementById('tool-bar').clientHeight + 15 + 'px'
-  document.getElementById('content').style.marginTop = newMargin
+  document.getElementById('content').style.marginTop = parseInt(newMargin) - 13 + 'px'
 
   let sideBar = document.getElementById('sidebarContent')
   if (sideBar) {
-    sideBar.style.top = parseInt(newMargin) + 10 + 'px'
+    sideBar.style.top = parseInt(newMargin) - 13 + 'px'
     sideBar.style.height =
-      document.documentElement.clientHeight - document.getElementById('optionsArea').clientHeight - 7 + 'px'
+      document.documentElement.clientHeight - document.getElementById('tool-bar').clientHeight + 'px'
   }
   let mainContent = document.getElementById('mainContent')
   if (mainContent) {
     mainContent.style.height =
-        document.documentElement.clientHeight - document.getElementById('tool-bar').clientHeight - 14 + 'px'
+        document.documentElement.clientHeight - document.getElementById('tool-bar').clientHeight + 'px'
   }
 }
