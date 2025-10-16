959ef5d60872fe13454644973232c691831d3fce
metelkin <evgeny.metelkin@gmail.com>
2025-10-16 06:15:16 +0000

    Deploying to gh-pages from @ insysbio/SbmlViewer@bfc693ee6608ee9d81e1ada27ae1ae728c66c890 🚀

---
+++ online/static/js/mathjax-config.js
@@ -0,0 +1,13 @@
+MathJax.Hub.Config({
+    jax: ["input/MathML", "output/HTML-CSS"],
+    extensions: ["mml2jax.js", "MathML/content-mathml.js", "MathZoom.js"],
+    menuSettings: { zoom: "Click" },
+    CommonHTML: {linebreaks: { automatic: true, width: "container" }},
+    "HTML-CSS": {
+        linebreaks: { automatic: true, width: "container" },
+        EqnChunk: 1,
+        EqnChunkFactor: 1,
+        EqnChunkDelay: 1
+    },
+    SVG: { linebreaks: { automatic: true, width: "container" } }
+});
